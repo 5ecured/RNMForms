450573c65069f32ab4b2dc85634097cf38e2c06a
Edward <onirotto7585@pm.me>
2025-08-22 10:19:28 +0700

    payment screen

--- src/app/checkout/payment.tsx
+++ src/app/checkout/payment.tsx
@@ -3,9 +3,27 @@ import React from 'react'
 import CustomButton from '../../components/CustomButton'
 import { router } from 'expo-router'
 import KeyboardAwareScrollView from '../../components/KeyboardAwareScrollView'
+import CustomTextInput from '../../components/CustomTextInput'
+import { FormProvider, useForm, SubmitHandler } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+
+// 1. For Zod validation, first define the schema and pass it to the useForm below
+const PaymentInfoSchema = z.object({
+  cardNumber: z.string().length(16),
+  expireDate: z.string().regex(/^(0[1-9]|1[0-2])\/?([0-9]{2})$/, 'Please use the MM/YY format'),
+  cvv: z.coerce.number().min(100).max(999)
+})
+
+// 2. Create the type to be passed to useForm and to onNext
+type PaymentInfo = z.infer<typeof PaymentInfoSchema>
 
 const PaymentDetailsForm = () => {
-  const onNext = () => {
+  const form = useForm<PaymentInfo>({
+    resolver: zodResolver(PaymentInfoSchema)
+  })
+
+  const onNext: SubmitHandler<PaymentInfo> = (data) => {
     // validate form
 
     // then, go next page
@@ -14,8 +32,29 @@ const PaymentDetailsForm = () => {
 
   return (
     <KeyboardAwareScrollView>
-      <Text>PaymentDetailsForm</Text>
-      <CustomButton title='Next' style={styles.button} onPress={onNext} />
+      <FormProvider {...form}>
+        <CustomTextInput
+          name='cardNumber'
+          label='Card number'
+          placeholder='1234 1234 1234 1234'
+        />
+        <View style={{ flexDirection: 'row', gap: 5 }}>
+          <CustomTextInput
+            name='expireDate'
+            label='Expiry date'
+            placeholder='05/25'
+            containerStyle={{ flex: 1 }}
+          />
+          <CustomTextInput
+            name='cvv'
+            label='CVV'
+            placeholder='123'
+            containerStyle={{ flex: 1 }}
+            inputMode='numeric'
+          />
+        </View>
+        <CustomButton title='Next' style={styles.button} onPress={form.handleSubmit(onNext)} />
+      </FormProvider>
     </KeyboardAwareScrollView>
   )
 }
@@ -29,6 +68,5 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: 'auto',
-    marginBottom: 25
   }
 })
